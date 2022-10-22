c1ce83ed305ada2e985d54245d71bd8ed34869a4
Bitee-cd <Calebduniya45@gmail.com>
2022-10-22 07:45:53 +0100

    convert files to json

--- wikipeedia/wikipedia.py
+++ wikipeedia/wikipedia.py
@@ -1,24 +1,28 @@
 #imports
-from contextlib import nullcontext
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import json
+
+
 
 #definition of variables
 URL = "https://en.wikipedia.org/wiki/Telephone_numbers_in_Nigeria"
-PATH = "C:\chromedriver.exe"
+
 
 #start service and open the page
-service= Service(PATH)
-service.start()
-driver = webdriver.Remote(service.service_url)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(URL)
-
+driver.maximize_window()
 
 #try connecting to the element with classname wikitable
+array_numbers={}
 try:
     section= WebDriverWait(driver,5).until(
         EC.presence_of_element_located((By.CLASS_NAME,"wikitable"))
@@ -26,7 +30,7 @@ try:
 
     #connect to tbody tag in the section element
     table_data = section.find_elements(By.TAG_NAME,"tbody")
-    array_numbers={}
+  
     
     for i in table_data:
 
@@ -40,8 +44,14 @@ try:
                     array_numbers[next]=[] 
                     array_numbers[next].append(i)
 
-    print(array_numbers)
+   
+    # Serializing json
+
     
             
 finally:
+    if array_numbers:
+        with open("example.json", "w") as json_obj:
+            json.dump(array_numbers, json_obj)
+    print("data created")
     driver.quit()
